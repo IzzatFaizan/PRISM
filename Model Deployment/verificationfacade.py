95fc1a036cb1bdfbf5f45566ff9673d347a59ef7
Izzat Faizan <izzatfaiz1996@gmail.com>
2018-12-25 02:26:23 +0800

    commit

--- Model Deployment/verificationfacade.py
+++ Model Deployment/verificationfacade.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
 
 @app.route('/api/verificationfacade/url/<string:url>', methods=['GET'])
